35680a6fe16e510d80d5617e067ac44333617476
Warrick FitzGerald <wfitzgerald@Warricks-MacBook-Pro.local>
2022-04-26 17:54:23 -0600

    Changes to that extnal prject dont have to path directly in order to get mapChainError

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as HelpersEos from './plugin/helpers'
 import * as ModelsEos from './plugin/models'
 import * as TemplateModelsEos from './plugin/templates/models'
 import Plugin from './plugin/ChainEosV2'
+import { mapChainError } from './plugin'
 
-export { HelpersEos, ModelsEos, TemplateModelsEos, Plugin }
+export { HelpersEos, ModelsEos, TemplateModelsEos, Plugin, mapChainError }
 
 // Note that the latest versions of node-fetch and eos-js break
