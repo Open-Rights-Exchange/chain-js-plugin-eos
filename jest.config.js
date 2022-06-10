62096cefb2042e240f25d59793fbc9a552aed591
Warrick FitzGerald <warrick@aikon.com>
2022-06-10 16:00:21 -0400

    Fix Jest Linter problems

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,21 @@
-var fs = require('fs')
+const fs = require('fs')
 
 function makeModuleNameMapper() {
-  const aliases = {};
+  const aliases = {}
   const { paths } = JSON.parse(fs.readFileSync('./tsconfig-paths.json')).compilerOptions
 
   // Iterate over paths and convert them into moduleNameMapper format
-  Object.keys(paths).forEach((item) => {
-      const key = item.replace('/*', '/(.*)');
-      const path = paths[item][0].replace('/*', '/$1');
-      aliases[key] = '<rootDir>/' + path;
-  });
-  return aliases;
+  Object.keys(paths).forEach(item => {
+    const key = item.replace('/*', '/(.*)')
+    const path = paths[item][0].replace('/*', '/$1')
+    aliases[key] = `<rootDir>/${path}`
+  })
+  return aliases
 }
 
-module.exports =  {
+module.exports = {
   automock: false,
-  roots: [
-    '<rootDir>/tests',
-    '<rootDir>/src/plugin/tests'
-  ],
+  roots: ['<rootDir>/tests', '<rootDir>/src/plugin/tests'],
   setupFiles: ['<rootDir>/tests/setupJest.js'],
   testMatch: ['<rootDir>/src/**/?(*.)(spec|test).(js|ts|tsx)'],
   transform: {
@@ -33,5 +30,5 @@ module.exports =  {
       },
     },
   },
-  moduleNameMapper: makeModuleNameMapper()
+  moduleNameMapper: makeModuleNameMapper(),
 }
