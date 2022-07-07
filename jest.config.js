eb52735fee0ce14f82d1ab73ca1c155758949199
Bruno Motta <wadge.motta@gmail.com>
2022-07-07 17:42:41 -0300

    fixing config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
   },
   transformIgnorePatterns: ['/node_modules/(?!eos-transit).+\\.js$'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
   moduleFileExtensions: ['web.js', 'js', 'json', 'web.jsx', 'jsx', 'ts', 'tsx', 'node'],
   modulePaths: ['<rootDir>/src/'],
   testMatch: ['<rootDir>/**/?(*.)(spec|test).(js|jsx|ts|tsx)'],
